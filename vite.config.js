99109e5ac1fb3ce50bbf9f2e3592d96f54940dd7
Parthik10 <156055046+Parthik10@users.noreply.github.com>
2025-03-28 09:23:57 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/", // Important for Vercel
   plugins: [react()],
-  base: "./", // Use relative paths for assets
-  build: {
-    outDir: "dist",
-  },
 });
